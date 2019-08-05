470616e0a8025d2a5d24f2e2bf2b266ff3765c79
langmomo <langmomo147@gmail.com>
2019-08-05 10:16:27 -0400

    update

--- src/main/java/Graph/TopologicalSort.java
+++ src/main/java/Graph/TopologicalSort.java
@@ -4,7 +4,46 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TopologicalSort {
+
+    //leetcode
+
+    //for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
     //DAG  directed acyclic graph
+
+    public static boolean courseSchedule(int numCourses, int[][] prerequisites){
+        int[] indegree = new int[numCourses];
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+        for(int i=0; i<prerequisites.length; i++){
+            indegree[prerequisites[i][0]]++;
+            HashSet<Integer> set = graph.getOrDefault(prerequisites[i][1], new HashSet<>());
+            set.add(prerequisites[i][0]);
+            graph.put(prerequisites[i][1], set);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int j=0; j<numCourses; j++){
+            if(indegree[j]==0){
+                q.offer(j);
+            }
+        }
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            HashSet<Integer> set = graph.getOrDefault(cur, new HashSet<>());
+            for(Integer curInt: set){
+                indegree[curInt]-=1;
+                //push to queue
+                if(indegree[curInt] ==0){
+                    q.offer(curInt);
+                }
+            }
+        }
+        for(int j=0; j<numCourses; j++){
+            if(indegree[j]!=0){
+                return false;
+            }
+        }
+        return true;
+
+    }
 
     public void generate(int number, int[][] edges){
         //generate graph
